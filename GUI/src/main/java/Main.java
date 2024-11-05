f3c69f995e19b193820f276e16f01cf8f66844ed
Ghiyath <ghiyathal101@gmail.com>
2024-11-05 16:05:27 +0100

    logic for GUI

--- GUI/src/main/java/Main.java
+++ GUI/src/main/java/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/path/to/your/EditView.fxml"));
+        Pane root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Property Management");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
