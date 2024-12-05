bc166d021b0b8df38542baa10ef8710ee4d78e1c
FREDERIK DIAMOND <hello@frederikhandberg.com>
2024-12-05 15:53:52 +0100

    Update Login.cs

--- Blazor/RealEstateBlazor/Data/Models/Login.cs
+++ Blazor/RealEstateBlazor/Data/Models/Login.cs
@@ -6,4 +6,5 @@ public class Login
     public string Username { get; set; }
     public string Password { get; set; }
     public string Role { get; set; } = "USER"; 
+    public UserProfile UserProfile { get; set; }
 }
