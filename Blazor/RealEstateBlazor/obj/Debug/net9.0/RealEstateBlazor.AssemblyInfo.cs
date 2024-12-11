299499e42394dc4b20c2da680c56cd31905edce9
Elham541 <elham54@hotmail.dk>
2024-12-11 16:08:45 +0100

    Added About and Contact page with css. Made few changes to overall aesthetics of the web page

--- Blazor/RealEstateBlazor/obj/Debug/net9.0/RealEstateBlazor.AssemblyInfo.cs
+++ Blazor/RealEstateBlazor/obj/Debug/net9.0/RealEstateBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstateBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aacfd085a14c9d5102f88d3d1aa8590534760de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f605fc3607170a0ad44e2d96c66d7fd86b682c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstateBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstateBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
